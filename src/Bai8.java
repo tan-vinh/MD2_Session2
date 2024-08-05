2581a9c949eb617f3938670e787656387fdf1fe2
tan-vinh <leecoongthanhfvinh@gmail.gmail.com>
2024-08-05 23:36:45 +0900

    post bai tap

--- src/Bai8.java
+++ src/Bai8.java
@@ -5,6 +5,7 @@ public class Bai8 {
         double a;
         double b;
         double c;
+        int choice;
 
         while (true) {
             Scanner sc = new Scanner(System.in);
@@ -16,10 +17,25 @@ public class Bai8 {
             c = sc.nextDouble();
 
             if (a + b > c && a + c > b && b + c > a) {
-                double s = (a + c + b)/2;
-                System.out.println("Chu vi tam giac la: " + (a + b + c));
-                System.out.println("Dien tich tam giac la: " + Math.sqrt(s * (s - a) * (s - b) * (s - c)));
-                break;
+                do {
+                    System.out.println("Nhập lựa chọn:");
+                    System.out.println("1. Tính chu vi:");
+                    System.out.println("2. Tính diện tích:");
+                    System.out.println("3. thoát.");
+                    choice = sc.nextInt();
+
+                    switch (choice) {
+                        case 1:
+                            System.out.println("Chu vi tam giac la: " + (a + b + c));
+                            break;
+                        case 2:
+                            double s = (a + c + b)/2;
+                            System.out.println("Dien tich tam giac la: " + Math.sqrt(s * (s - a) * (s - b) * (s - c)));
+                            break;
+                        case 3:
+                            System.exit(choice);
+                    }
+                }while (choice != 3);
             } else {
                 System.out.println("Khong hop le. Vui long nhap lai.");
             }
